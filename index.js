60c6f55cf52b0bf93b23acde090268f1dd221bbd
Daniel Doughty <djdoughty@gmail.com>
2018-04-04 17:25:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ function updateObjectWithKeyAndValue(object, key, value) {
 
 var a = updateObjectWithKeyAndValue(recipes, 'pie', 'pecan')
 console.log(a)
-// console.log (recipes)
+console.log (recipes)
 // console.log("hello world!")
